afe3fe7f7de326b6079faed4fe46e3993c7ebc23
Alan B. de Oliveira <jepridaz@gmail.com>
2024-07-07 22:10:08 -0300

    fix

--- qr_matrix_decomposition.rb
+++ qr_matrix_decomposition.rb
@@ -54,52 +54,12 @@ def cumulative_vector(a,e,j)
   return sum
 end
 
-#================================
 
-a = []
-u = []
-e = []
-r = []
-
-#a = [[12,6,-4],[-51,167,24],[4,-68,-41]]
-a = [[1,1,0],[1,0,1],[0,1,1]]
-#a = [[1,1,1],[1,2,3]]
-
-  @ncol = a.length
-  @nlin = a[0].length
-
-
-  j = 0
-  u[j] = a[j]
-  e[j] = director(u[j])
-
-
-
-  (1..@ncol-1).each do |j|
-    cv = cumulative_vector(a,e,j)
-    u[j] = sumsub(a[j],cv,-1)
-    e[j] = director(u[j])
-  end
-
-
-
-  @ncol.times do |i|
-    tmp = []
-    @ncol.times do |j|
-      tmp[j] = inner(a[i],e[j]).round(4)
-    end
-    r[i] = tmp
-  end
-
-
-  puts "A:"
-  precision = 0
+def print_matrix(lines,columns,precision,array)
   matrix = ''
-  lines = @nlin
-  columns = @ncol
   lines.times do |i|
     columns.times do |j|
-      element = (a[j][i].round(precision)).to_s
+      element = (array[j][i].round(precision)).to_s
       element = "%.#{precision}f"%element
       space = precision + 5 - element.size
       if precision == 0 then space = 2 end
@@ -108,33 +68,49 @@ a = [[1,1,0],[1,0,1],[0,1,1]]
     matrix += "\n"
   end
   puts matrix+"\n"
+end
 
+#================================
+
+a = [[1,1,0],[1,0,1],[0,1,1]]
+
+u = []
+e = []
+r = []
+
+
+@ncol = a.length
+@nlin = a[0].length
+
+
+j = 0
+u[j] = a[j]
+e[j] = director(u[j])
+
+
+
+(1..@ncol-1).each do |j|
+  cv = cumulative_vector(a,e,j)
+  u[j] = sumsub(a[j],cv,-1)
+  e[j] = director(u[j])
+end
+
+
+
+@ncol.times do |i|
+  tmp = []
+  @ncol.times do |j|
+    tmp[j] = inner(a[i],e[j])
+  end
+  r[i] = tmp
+end
+
+
+puts "A:"
+print_matrix(@nlin,@ncol,0,a)
   
-  puts "Q:"
-  matrix = ''
-  @nlin.times do |i|
-    @ncol.times do |j|
-      element = (e[j][i].round(4)).to_s
-      space = 9 - element.size
-      matrix += " "*space+element
-    end
-    matrix += "\n"
-  end
-  puts matrix+"\n"
+puts "Q:"
+print_matrix(@nlin,@ncol,4,e)
 
-
-
-  puts "R:"
-  matrix = ''
-  @ncol.times do |i|
-    @ncol.times do |j|
-      element = (r[j][i].round(4)).to_s
-      space = 9-element.size
-      matrix += " "*space+element
-    end
-    matrix += "\n"
-  end
-  puts matrix
-
-
-
+puts "R:"
+print_matrix(@ncol,@ncol,4,r)
